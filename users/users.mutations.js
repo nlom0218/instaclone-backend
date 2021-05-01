02cdd9fc01cbf1bbd4c51428695a9556c46aad14
nlom0218 <nlom0218@naver.com>
2021-05-01 19:02:11 +0900

    seeProfile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -4,20 +4,26 @@ import bcrypt from "bcrypt"
 export default {
     Mutation: {
         createAccount: async (_, { firstName, lastName, username, email, password, }) => {
-            // check if username or email are already on DB
-            const existingUser = await client.user.findFirst({
-                where: {
-                    OR: [{ username }, { email }]
-                }
-            })
-            // hash password
-            const uglyPassword = await bcrypt.hash(password, 10)
-            // save and return the user
-            return client.user.create({
-                data: {
-                    username, email, firstName, lastName, password: uglyPassword
-                }
-            })
+            try {
+                // check if username or email are already on DB
+                const existingUser = await client.user.findFirst({
+                    where: {
+                        OR: [{ username }, { email }]
+                    }
+                })
+                if (existingUser) { throw new Error("이메일 혹은 이름이 이미 존재합니다.") }
+                console.log(existingUser);
+                // hash password
+                const uglyPassword = await bcrypt.hash(password, 10)
+                // save and return the user
+                return client.user.create({
+                    data: {
+                        username, email, firstName, lastName, password: uglyPassword
+                    }
+                })
+            } catch (err) {
+                return err
+            }
         }
     }
 }
