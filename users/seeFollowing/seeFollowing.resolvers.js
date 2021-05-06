faa56392249647b87320fc7cdf6a84c05b04e2c5
nlom0218 <nlom0218@naver.com>
2021-05-06 21:40:31 +0900

    Following Pagination

---
+++ users/seeFollowing/seeFollowing.resolvers.js
@@ -0,0 +1,31 @@
+import client from "../../client";
+
+export default {
+    Query: {
+        seeFollowing: async (_, { username, lastId }) => {
+            try {
+                const ok = await client.user.findUnique({ where: { username }, select: { id: true } })
+                console.log(ok);
+                if (!ok) {
+                    return {
+                        ok: false,
+                        error: "유저가 존재하지 않습니다."
+                    }
+                }
+                const following = await client.user
+                    .findUnique({ where: { username } })
+                    .following({
+                        take: 5,
+                        skip: lastId ? 1 : 0,
+                        ...(lastId && { cursor: { id: lastId } })
+                    })
+                return {
+                    ok: true,
+                    following
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
+}
