e0d380a0d94293032a1a29c8da5f1cad6b8da37f
nlom0218 <nlom0218@naver.com>
2021-05-07 20:00:55 +0900

    Computed Fields

---
+++ users/users.resolvers.js
@@ -0,0 +1,38 @@
+import client from "../client"
+
+export default {
+    User: {
+        totalFollowing: ({ id }) => {
+            return client.user.count({
+                where: { followers: { some: { id } } }
+            })
+        },
+        totalFollowers: ({ id }) => {
+            return client.user.count({
+                where: { following: { some: { id } } }
+            })
+        },
+        isMe: ({ id }, _, { loggedInUser }) => {
+            if (!loggedInUser) {
+                return false
+            }
+            return id === loggedInUser.id
+        },
+        isFollowing: async ({ id }, _, { loggedInUser }) => {
+            if (!loggedInUser) {
+                return false
+            }
+            try {
+                const exists = await client.user.count({
+                    where: {
+                        username: loggedInUser.username,
+                        following: { some: { id } }
+                    }
+                })
+                return Boolean(exists)
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
+}
