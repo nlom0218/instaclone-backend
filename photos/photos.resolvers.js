caed3befc38a0c0ef33b26b52132c2685c69340d
nlom0218 <nlom0218@naver.com>
2021-05-08 13:48:38 +0900

    seePhoto

---
+++ photos/photos.resolvers.js
@@ -0,0 +1,9 @@
+import client from "../client"
+
+export default {
+    Photo: {
+        user: ({ userId }) => client.user.findUnique({ where: { id: userId } }),
+        hashtags: ({ id }) => client.hashtag
+            .findMany({ where: { photos: { some: { id } } } })
+    }
+}
